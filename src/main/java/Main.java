412f13dd71ecdcff7856ba117ac114cdfe3bf10e
YmerA38 <stin166c@stud.kea.dk>
2022-09-26 12:31:11 +0200

    her stinus

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import com.sun.security.jgss.GSSUtil;
+
 public class Main {
 
 public static void main(String[] args){
@@ -8,6 +10,8 @@ public static void main(String[] args){
     System.out.println("heyyyyyyyyyy");
 
     System.out.println("stinus her");
+
+    System.out.println("nu skriver jeg bare ....");
 }
 
 }
