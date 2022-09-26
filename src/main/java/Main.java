bfc50f91917722af7da7ed4b3e1793ab8183b506
frannerkea <chri268u@stud.kea.dk>
2022-09-26 12:24:37 +0200

    2. lavet til stort i Wolrd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
 public static void main(String[] args){
     System.out.println("Hello World");
 
-    System.out.println("Hello wolrd 2");
+    System.out.println("Hello Wolrd 2");
 }
 
 }
